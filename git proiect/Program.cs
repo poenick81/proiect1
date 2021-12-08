39ff81979a826db6fd73390bd74d5ab6dad86627
nick <poeick81@yahoo.com>
2021-12-08 15:47:03 +0100

    Added message on screen

--- git proiect/Program.cs
+++ git proiect/Program.cs
@@ -10,6 +10,8 @@ namespace git_proiect
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
         }
     }
 }
